c0add082e8a417e79f2b81b3914a2667d078c195
ClarissaPaitanS <clarypaitan@gmail.com>
2020-07-14 12:05:51 -0500

    Creando .babelrc y .eslintrc

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -3,12 +3,12 @@ import validator from '../src/validator';
 
 describe('validator', () => {
   it('debería ser un objeto', () => {
-    expect(typeof validator).toBe('object');
+  expect(typeof validator).toBe('object');
   });
 
   describe('validator.isValid', () => {
     it('debería ser una función', () => {
-      expect(typeof validator.isValid).toBe('function');
+    expect(typeof validator.isValid).toBe('function');
     });
 
     it.skip('debería retornar true para "4083952015263"', () => {
@@ -26,7 +26,7 @@ describe('validator', () => {
 
   describe('validator.maskify', () => {
     it('debería ser una función', () => {
-      expect(typeof validator.maskify).toBe('function');
+    expect(typeof validator.maskify).toBe('function');
     });
 
     it.skip('Debería retornar "############5616" para "4556364607935616"', () => {
