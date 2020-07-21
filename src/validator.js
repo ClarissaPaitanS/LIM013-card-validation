6cc2397b875f3da8755bae9fa56cbfed37f870f0
ClarissaPaitanS <clarypaitan@gmail.com>
2020-07-21 09:49:26 -0500

    Avance Modificando codigo función isValid

--- src/validator.js
+++ src/validator.js
@@ -1,27 +1,50 @@
 const validator = {
 
-  isValid: function isValid(numero_tarjeta) {
-        
+  isValid: function isValid(numero_tarjeta) {  
     const cadena = numero_tarjeta.toString();
     const longitud = cadena.length;
     let cifra = 0;
     let cifra_cad=0;
     let suma=0;
     
-    for (let i=0; i < longitud; i+=2){
-      cifra = parseInt(cadena.charAt(i))*2;
-
-      if (cifra > 9){ 
-        cifra_cad = cifra.toString();
-        cifra = parseInt(cifra_cad.charAt(0)) + 
-          parseInt(cifra_cad.charAt(1));
+    if ((longitud%2)===0) {
+      for (let i=0; i < longitud; i+=2){
+        cifra = parseInt(cadena.charAt(i))*2;
+  
+        if (cifra > 9){ 
+          cifra_cad = cifra.toString();
+          cifra = parseInt(cifra_cad.charAt(0)) + 
+            parseInt(cifra_cad.charAt(1));
+        }
+            suma+=cifra;
       }
-          suma+=cifra;
+  
+      for (let i=1; i < longitud; i+=2){
+        suma += parseInt(cadena.charAt(i));
+      }
+    
+    }else{
+      for (let i=1; i < longitud; i+=2){
+        cifra = parseInt(cadena.charAt(i))*2;
+  
+        if (cifra > 9){ 
+          cifra_cad = cifra.toString();
+          cifra = parseInt(cifra_cad.charAt(0)) + 
+            parseInt(cifra_cad.charAt(1));
+        }
+            suma+=cifra;
+      }
+  
+      for (let i=0; i < longitud; i+=2){
+        suma += parseInt(cadena.charAt(i));
+      }
     }
 
-    for (let i=1; i < longitud; i+=2){
-      suma += parseInt(cadena.charAt(i));
-    }
+
+
+
+
+
 
     if ((suma % 10) === 0){ 
     return true;
