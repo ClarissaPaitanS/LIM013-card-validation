46c5226bbe0e591534c1cab44ccb1757338cae3f
ClarissaPaitanS <clarypaitan@gmail.com>
2020-07-26 17:42:02 -0500

    Modificación Codigo JavaScript

--- src/index.js
+++ src/index.js
@@ -5,129 +5,142 @@ const btnAdquirir = document.getElementById("btnAdquirir");
 const span = document.getElementsByClassName("close")[0];
 
 // Abrir la ventana modal - Formulario Pago
-    btnAdquirir.onclick = function() {
-        modalPago.style.display = "block";
-    }
+  // btnAdquirir.onclick = function() {
+  //modalPago.style.display = "block";
+  // modalPago.classList.add("mostrar");
+  // }
+
+  btnAdquirir.addEventListener('click',function () {
+    modalPago.classList.add("mostrar");
+  })
+
 // Cerrar la ventana modal - Pago
-    span.onclick = function() {
-        modalPago.style.display = "none";
-    }
+  //span.onclick = function() {
+  //modalPago.style.display = "none";
+  //  modalPago.classList.remove("mostrar");
+  // }
+  span.addEventListener('click',function () {
+    modalPago.classList.remove("mostrar");
+  })
 
   // Cliquear fuera y cerrar ventana modal - Pago
-    window.onclick = function(event) {
-        if (event.target == modalPago) {
-    modalPago.style.display = "none";
-        }
-    }
+  //window.onclick = function(event) {
+  // if (event.target == modalPago) {
+  //modalPago.style.display = "none";
+  //modalPago.classList.remove("mostrar");
+  // }
+  // }
+  window.addEventListener('click',function () {
+    if (event.target == modalPago) {
+    modalPago.classList.remove("mostrar");
+  } 
+  })
 
-//Pagar.onclick llamará función realizarPago.
-document.getElementById("pagar").onclick=realizarPago;
-document.getElementById("CreditCard").onclick=limpiarCampos;
+//document.getElementById("pagar").onclick=realizarPago;
+//document.getElementById("CreditCard").onclick=limpiarCampos;
+const btnPagar=document.getElementById("pagar");
+btnPagar.addEventListener('click',realizarPago);
 
+const limpiarDatos=document.getElementById("CreditCard");
+limpiarDatos.addEventListener('click',limpiarCampos);
 
 /* Función para Realizar Pago: */
 function realizarPago() {
-    //Variables:
-    const creditCard = document.getElementById("CreditCard").value;
-    const nameClient=document.getElementById("NameClient").value;
-    const descargar = document.getElementById("detalle");
-    const mensajeA= document.getElementById("mensajeA");
-    const mensajeC= document.getElementById("mensajeC");
-
+  //Variables:
+  const creditCard = document.getElementById("CreditCard").value;
+  const nameClient=document.getElementById("NameClient").value;
+  const descargar = document.getElementById("detalle");
+  const mensajeA= document.getElementById("mensajeA");
+  const mensajeC= document.getElementById("mensajeC");
 
 if (validarDatos()!==false) {
 
   if ( validator.isValid(creditCard)!==false) {
-    mensajeA.style.display="none";
-    mensajeC.style.display="inline";
-    alert("Número de tarjeta es Válido");
-            
-            setTimeout(() => {
-                modalPago.style.display="none";
-                modalDetalle.style.display="block"
-            }, 2000);
-        
+  mensajeA.classList.remove("visible");
+  mensajeC.classList.add("visible");
+ // mensajeA.style.display="none";
+ // mensajeC.style.display="inline";
+  alert("Número de tarjeta es Válido");
+    setTimeout(() => {
+    //modalPago.style.display="none";
+    modalPago.classList.remove("mostrar");
+    //modalDetalle.style.display="block"
+    modalDetalle.classList.add("mostrar");
+    }, 2000);
+  
+    descargar.innerHTML=
+    '<h1>¡Tu Pago se Realizo con Éxito!</h1>'+
+    '<br/><img src="img/gracias.gif" alt="gracias">'+
+    '<p>Gracias <b>'+ nameClient +', </b> por tu compra.</p>'+ 
+    '<p><b> Nro. Tarjeta: </b>'+ validator.maskify(creditCard) +'</p>'+
+    '<p><b> Puedes Iniciar la Descarga de la Imagen en Alta Calidad.</b></p>'+
+    '<p> Esperamos volver a verte <b>¡Pronto! </b></p>'+
+    '<p><a href="img/fotografia.jpg" download>'+
+    '<img src="img/download.png" alt="download" width="32" height="32" onclick="location.reload();">'+
+    '</a></p>';
+    }
+    else{
+    //mensajeA.style.display="inline";
+    mensajeA.classList.add("visible");
+    alert("Número de tarjeta no es Válido");
+    }
 
-
-            descargar.innerHTML=
-            '<h1>¡Tu Pago se Realizo con Éxito!</h1>'+
-            '<br/><img src="img/gracias.gif" alt="gracias">'+
-            '<p>Gracias <b>'+ nameClient +', </b> por tu compra.</p>'+ 
-            '<p><b> Nro. Tarjeta: </b>'+ validator.maskify(creditCard) +'</p>'+
-            '<p><b> Puedes Iniciar la Descarga de la Imagen en Alta Calidad.</b></p>'+
-            '<p> Esperamos volver a verte <b>¡Pronto! </b></p>'+
-            '<p><a href="img/fotografia.jpg" download>'+
-            '<img src="img/download.png" alt="download" width="32" height="32" onclick="location.reload();">'+
-            '</a></p>';
-            }
-            else{
-                mensajeA.style.display="inline";
-                alert("Número de tarjeta no es Válido");
-            }
-
-       // console.log(validator.maskify(creditCard)); /*4137894711755904*/
-       // eslint-disable-next-line no-console
-        console.log(validator.maskify(creditCard));
+    // console.log(validator.maskify(creditCard)); /*4137894711755904*/
+    // eslint-disable-next-line no-console
+    console.log(validator.maskify(creditCard));
+  }
 }
-
-}
-
-
-
-
 
 /* Función para Validar Campos: */
 function validarDatos() {
-    //Variables:
-    const  creditCard = document.getElementById("CreditCard").value;
-    const  nameClient=document.getElementById("NameClient").value;
-    const  mesExpira=document.getElementById("MesExpira").value;
-    const  yearExpira=document.getElementById("YearExpira").value;
-    const  codCVV=document.getElementById("CVV").value;
-    
-    if (nameClient==="" || creditCard==="" || mesExpira==="" || yearExpira==="" || codCVV==="") {
-        alert("Todos los Campos son Obligatorios");
-        return false;
-    }
-    else if (nameClient.length>120) {
-        alert("El Nombre del Titular es Muy Largo");
-        return false;
-    }
-    else if (creditCard.length!==16) {
-        alert("El Nro. de Tarjeta debe Contener 16 Digitos");
-        return false; 
-    }
-    else if (mesExpira.length!==2 || yearExpira.length!==2) {
-        alert("El Mes y Año debe Contener 2 Digitos.");
-        return false; 
-    }
-    else if (codCVV.length!==3) {
-        alert("El CVV debe Contener 3 Digitos.");
-        return false; 
-    }
-    else if (isNaN(creditCard) || isNaN(mesExpira) || isNaN(yearExpira) || isNaN(codCVV)) {
-        alert("El Dato Ingresado no es un Número");
-        return false; 
-    }
-    else if (mesExpira<0 || mesExpira>12) {
-        alert("El Mes Ingresado, no es Correcto");
-        return false; 
-    }
-    else if (yearExpira<20) {
-        alert("El Año Ingresado, no es Correcto o su Tarjeta ya Expiró");
-        return false; 
-    }
-    
-    }
+  //Variables:
+  const  creditCard = document.getElementById("CreditCard").value;
+  const  nameClient=document.getElementById("NameClient").value;
+  const  mesExpira=document.getElementById("MesExpira").value;
+  const  yearExpira=document.getElementById("YearExpira").value;
+  const  codCVV=document.getElementById("CVV").value;
 
+  if (nameClient==="" || creditCard==="" || mesExpira==="" || yearExpira==="" || codCVV==="") {
+    alert("Todos los Campos son Obligatorios");
+    return false;
+    }
+  else if (nameClient.length>120) {
+    alert("El Nombre del Titular es Muy Largo");
+    return false;
+  }
+  else if (creditCard.length!==16) {
+    alert("El Nro. de Tarjeta debe Contener 16 Digitos");
+    return false; 
+  }
+  else if (mesExpira.length!==2 || yearExpira.length!==2) {
+    alert("El Mes y Año debe Contener 2 Digitos.");
+    return false; 
+  }
+  else if (codCVV.length!==3) {
+    alert("El CVV debe Contener 3 Digitos.");
+    return false; 
+  }
+  else if (isNaN(creditCard) || isNaN(mesExpira) || isNaN(yearExpira) || isNaN(codCVV)) {
+    alert("El Dato Ingresado no es un Número");
+    return false; 
+  }
+  else if (mesExpira<0 || mesExpira>12) {
+    alert("El Mes Ingresado, no es Correcto");
+    return false; 
+  }
+  else if (yearExpira<20) {
+    alert("El Año Ingresado, no es Correcto o su Tarjeta ya Expiró");
+    return false; 
+  }
+  }
 
 /* Función para Limpiar Campos: */
 function limpiarCampos() {
-    document.getElementsByName("CreditCard")[0].value="";
-    document.getElementsByName("NameClient")[0].value="";
-    document.getElementsByName("MesExpira")[0].value="";
-    document.getElementsByName("YearExpira")[0].value="";
-    document.getElementsByName("CVV")[0].value="";
+  document.getElementsByName("CreditCard")[0].value="";
+  document.getElementsByName("NameClient")[0].value="";
+  document.getElementsByName("MesExpira")[0].value="";
+  document.getElementsByName("YearExpira")[0].value="";
+  document.getElementsByName("CVV")[0].value="";
 }
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,60 +1,54 @@
 const validator = {
 
   isValid: function isValid(numero_tarjeta) {  
-    const cadena = numero_tarjeta.toString();
-    const longitud = cadena.length;
-    let cifra = 0;
-    let cifra_cad=0;
-    let suma=0;
-    
-    if ((longitud%2)===0) {
+  const cadena = numero_tarjeta.toString();
+  const longitud = cadena.length;
+  let cifra = 0;
+  let cifra_cad=0;
+  let suma=0;
 
-      
-      for (let i=0; i < longitud; i+=2){
-        cifra = parseInt(cadena.charAt(i))*2;
-  
-        if (cifra > 9){ 
-          cifra_cad = cifra.toString();
-          cifra = parseInt(cifra_cad.charAt(0)) + 
-            parseInt(cifra_cad.charAt(1));
-        }
-            suma+=cifra;
-      }
-  
-      for (let i=1; i < longitud; i+=2){
-        suma += parseInt(cadena.charAt(i));
-      }
-    
-    }else{
-      for (let i=1; i < longitud; i+=2){
-        cifra = parseInt(cadena.charAt(i))*2;
-  
-        if (cifra > 9){ 
-          cifra_cad = cifra.toString();
-          cifra = parseInt(cifra_cad.charAt(0)) + 
-            parseInt(cifra_cad.charAt(1));
-        }
-            suma+=cifra;
-      }
-  
-      for (let i=0; i < longitud; i+=2){
-        suma += parseInt(cadena.charAt(i));
-      }
+  if ((longitud%2)===0) {
+    for (let i=0; i < longitud; i+=2){
+    cifra = parseInt(cadena.charAt(i))*2;
+    if (cifra > 9){ 
+      cifra_cad = cifra.toString();
+      cifra = parseInt(cifra_cad.charAt(0)) + 
+      parseInt(cifra_cad.charAt(1));
     }
+    suma+=cifra;
+    }
+  
+  for (let i=1; i < longitud; i+=2){
+    suma += parseInt(cadena.charAt(i));
+    }
+  }
+  else{
+    for (let i=1; i < longitud; i+=2){
+    cifra = parseInt(cadena.charAt(i))*2;
+    if (cifra > 9){ 
+    cifra_cad = cifra.toString();
+    cifra = parseInt(cifra_cad.charAt(0)) + 
+    parseInt(cifra_cad.charAt(1));
+    }
+    suma+=cifra;
+    }
+  
+  for (let i=0; i < longitud; i+=2){
+    suma += parseInt(cadena.charAt(i));
+    }
+  }
 
-    if ((suma % 10) === 0){ 
-    return true;
-  } else {
-      return false;
-      }
-    },
+  if ((suma % 10) === 0){ 
+  return true;
+  }
+  else {
+  return false;
+  }
+  },
 
 maskify: function maskify (numero_tarjeta) {
   numero_tarjeta= numero_tarjeta.replace(/.(?=.{4})/g, "#");
   return numero_tarjeta;
-}
-
-
-};
+}};
 
 export default validator;
